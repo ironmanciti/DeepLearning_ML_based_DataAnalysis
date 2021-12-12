77cd5f6ebeec0f812e7fc9241dd2b730b8e82e37
YJOh <trimurti@naver.com>
2021-12-12 12:28:38 +0900

    sunday

--- 902_mpg_server.py
+++ 902_mpg_server.py
@@ -1,34 +1,70 @@
-# Deploy simple Keras model with Flask only.
+from werkzeug.wrappers import Request, Response
 from flask import Flask, request, jsonify
+import uuid  #범용고유식별자 생성
 import os
-from tensorflow.keras.models import load_model
 import numpy as np
+from werkzeug.serving import run_simple
+from tensorflow.keras.models import load_model
+import pickle
 
 app = Flask(__name__)
 
-# Load neural network model
-model = load_model(os.path.join("./dnn/", "mpg_model.h5"))
+# 입력 data validation
+EXPECTED = {
+    "cylinders":{"min":3,"max":8},
+    "displacement":{"min":68.0,"max":455.0},
+    "horsepower":{"min":46.0,"max":230.0},
+    "weight":{"min":1613,"max":5140},
+    "acceleration":{"min":8.0,"max":24.8},
+    "year":{"min":70,"max":82},
+    "origin":{"min":1,"max":3}
+}
 
-@app.route('/api/mpg', methods=['POST'])
+# model and scaler load
+model = load_model("./dnn/mpg_model.h5")
+sc = pickle.load(open("./dnn/standard_scaler.pkl", "rb"))
+
+@app.route("/api/mpg/", methods=['POST'])
 def calc_mpg():
     content = request.json
-
-    # Predict
-    x = np.zeros((1, 7))
-
-    x[0, 0] = content['cylinders']
-    x[0, 1] = content['displacement']
-    x[0, 2] = content['horsepower']
-    x[0, 3] = content['weight']
-    x[0, 4] = content['acceleration']
-    x[0, 5] = content['year']
-    x[0, 6] = content['origin']
-
-    pred = model.predict(x)
-    mpg = float(pred[0])
-    response = {"mpg": mpg}
+    errors = []
+    #input validity check
+    for name in content:
+        if name in EXPECTED:
+            value = content[name]
+            if value < EXPECTED[name]['min'] or value > EXPECTED[name]['max']:
+                error.append(f"입력값 범위 초과 - {name}: {value}")
+        else:
+            error.append(f"Unexpected field - {name}")
+    # missing input check
+    for name in EXPECTED:
+        if name not in content:
+            errors.append(f"missing input - {name}")
+            
+    # prediction
+    if len(errors) < 1:
+        x = np.zeros((1, 7))
+        x[0,0] = content['cylinders'] 
+        x[0,1] = content['displacement']
+        x[0,2] = content['horsepower']
+        x[0,3] = content['weight']
+        x[0,4] = content['acceleration'] 
+        x[0,5] = content['year'] 
+        x[0,6] = content['origin']
+        
+        x_scaled = sc.transform(x)
+        pred = model.predict(x_scaled)
+        
+        mpg = float(pred[0])
+        
+        response = {"id": str(uuid.uuid4()), "mpg": mpg}
+    else:
+        response = {"id": str(uuid.uuid4()), "errors": errors}
+        
+    print(response)
 
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(host='localhost', debug=True)
+    run_simple('localhost', 9000, app)
+#     app.run(host='0.0.0.0', debug=True)
--- CapstoneProject_2_flask_image_app.py
+++ CapstoneProject_2_flask_image_app.py
@@ -10,7 +10,7 @@ import io
 
 app = Flask(__name__)
 model = None
-
+# pre-trained model load
 def load_model():
     global model
     model = VGG16(weights='imagenet', include_top=True)
@@ -22,25 +22,26 @@ def upload_file():
         if request.files.get('file'):  # image is stored as name "file"
             img_requested = request.files['file'].read()
             img = Image.open(io.BytesIO(img_requested))
+            #VGG16 입력 사양에 맞추어 전처리
             if img.mode != 'RGB':
                 img = img.convert('RGB')
             img = img.resize((224, 224))
             img = image.img_to_array(img)
             img = np.expand_dims(img, axis=0)
             inputs = preprocess_input(img)
-
+            # Predict
             preds = model.predict(inputs)
             results = decode_predictions(preds)
 
             response['predictions'] = []
             # [0] as input is only one image
             for (imagenetID, label, prob) in results[0]:
-                # numpy float is not good for json
+                # output formatting
                 row = {'label': label, 'probability': float(prob)}
                 response['predictions'].append(row)
             response['success'] = True
             return jsonify(response)
-    else: # GET
+    else: # GET 인 경우 POST method HTML 페이지 전송
         return '''
             <!doctype html>
             <title>Upload new File</title>
@@ -50,7 +51,6 @@ def upload_file():
                 <input type=submit value=Upload>
             </form>
             '''
-
 
 if __name__ == '__main__':
     load_model()
